257f3b9f353577353a80a5b5d2def0ddd59d6c7f
Spencer <spencer_zezulka@berkeley.edu>
2023-12-06 02:15:23 -0800

    Added hooks

--- app/examples/scrape_examples.py
+++ app/examples/scrape_examples.py
@@ -6,41 +6,44 @@ import os
 
 # Run script with link to well-summarized video to generate few-shot example
 
+
 def main():
     # 1. Ensure standardized filepath
-    json_file_path = 'examples.json'
+    json_file_path = "examples.json"
 
     # 2. Handle case of JSON not (yet) in existence
     if os.path.exists(json_file_path):
-        with open(json_file_path, 'r') as file:
+        with open(json_file_path, "r") as file:
             examples = json.load(file)
     else:
         examples = []
 
     # 3. Prompt user for link
-    video_url = input('Enter valid YouTube URL: ')
-    
+    video_url = input("Enter valid YouTube URL: ")
+
     # 4. Terminate if link already in dataset
-    if any(example['link'] == video_url for example in examples):
-        raise ValueError('Link already exists in examples.json. Terminating.')
+    if any(example["link"] == video_url for example in examples):
+        raise ValueError("Link already exists in examples.json. Terminating.")
 
     # 5. Prompt user for well-formatted summary
-    def get_multiline_input(editor='nano'):
+    def get_multiline_input(editor="nano"):
         # Instruct user
-        print("You will be prompted to enter your well-formatted summary in the nano text editor. After you're done, save and exit to continue.")
-        
+        print(
+            "You will be prompted to enter your well-formatted summary in the nano text editor. After you're done, save and exit to continue."
+        )
+
         # Wait for user to read instructions
         input("Press <Enter> to continue...")
 
         # Use a temporary file to store user input
-        with tempfile.NamedTemporaryFile(mode='r+', delete=False) as tmpfile:
+        with tempfile.NamedTemporaryFile(mode="r+", delete=False) as tmpfile:
             tmpfile_path = tmpfile.name
 
             # Open the editor
             subprocess.run([editor, tmpfile_path])
 
             # Read the content of the file
-            with open(tmpfile_path, 'r') as tmpfile:
+            with open(tmpfile_path, "r") as tmpfile:
                 user_input = tmpfile.read()
 
             # Optionally delete the temporary file if you want to clean up
@@ -51,23 +54,19 @@ def main():
     user_summary = get_multiline_input()
 
     # 6. Load metadata
-    loader = YoutubeLoader.from_youtube_url(
-            video_url, add_video_info=True
-        )
+    loader = YoutubeLoader.from_youtube_url(video_url, add_video_info=True)
     youtube_metadata = loader.load()[0].dict()
 
     # 5. Add example, given that link is not already in data
     new_entry = {
-        'link': video_url,
-        'fewshotmapping': {
-            'metadata': youtube_metadata,
-            'summary': user_summary
-        }
+        "link": video_url,
+        "fewshotmapping": {"metadata": youtube_metadata, "summary": user_summary},
     }
     examples.append(new_entry)
-    with open(json_file_path, 'w') as file:
+    with open(json_file_path, "w") as file:
         json.dump(examples, file, indent=4)
-    print('Entry to examples.json successful!')
+    print("Entry to examples.json successful!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- app/main.py
+++ app/main.py
@@ -1,20 +1,17 @@
-from pyexpat import model
 import streamlit as st
 from langchain.document_loaders import YoutubeLoader
-from langchain.chains.combine_documents.stuff import StuffDocumentsChain
-from langchain.chains.summarize import load_summarize_chain
 import os
 from dotenv import load_dotenv, find_dotenv
 from utils.langchain_utils import generate_summary
 from utils.streamlit_utils import double_newlines
 
 
-def load_api_key() -> str: 
+def load_api_key() -> str:
     load_dotenv(find_dotenv())
     # Escape entering api key in UI if .env file provided with api key
-    if os.environ.get('OPEN_AI_KEY'):
-        api_key = os.environ.get('OPEN_AI_KEY')
-    
+    if os.environ.get("OPEN_AI_KEY"):
+        api_key = os.environ.get("OPEN_AI_KEY")
+
     # If no valid API key provided in .env, request through UI
     else:
         with st.sidebar:
@@ -26,58 +23,78 @@ def load_api_key() -> str:
             )
     return api_key
 
+
 # UI logic
 def main():
     # App title
-    st.set_page_config(
-        layout='wide', page_title='YouTube Video Summary')
+    st.set_page_config(layout="wide", page_title="YouTube Video Summary")
     st.title("YouTube Video Summary Bot :robot_face:")
 
     api_key = load_api_key()
     if not api_key:
-        with st.sidebar(key='no_api_key'):
-            st.write('You must please provide a valid OpenAI API key in the sidebar to access this app.')
+        with st.sidebar(key="no_api_key"):
+            st.write(
+                "You must please provide a valid OpenAI API key in the sidebar to access this app."
+            )
         exit()
-    
+
     # Input form for the YouTube link
 
     if api_key:
         with st.sidebar:
             # Model selection
-            model_names = ('gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 
-                'gpt-4', 'gpt-4-32k', 'gpt-4-1106-preview') # defaults to prereleased gpt-4
-            model_information = '''
+            model_names = (
+                "gpt-3.5-turbo",
+                "gpt-3.5-turbo-16k",
+                "gpt-4",
+                "gpt-4-32k",
+                "gpt-4-1106-preview",
+            )  # defaults to prereleased gpt-4
+            model_information = """
             Larger models (size is denoted by the suffix, e.g. '-16k', '-32k') can read in more tokens at once,
             and thus can read in larger videos as a whole, but also tend to cost more. All else equal, using gpt-4 costs more 
             than using gpt-3.5. gpt-4-1106, at the time of writing, has a 128k token context window
             and is faster.
-            '''
-            best_default_model_index = len(model_names)-1
-            user_selected_model = st.selectbox('Select model', options=model_names, help=model_information, index=best_default_model_index)
-            st.markdown('For more information on models and their costs, check the [OpenAI API Models page](https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo)')
+            """
+            best_default_model_index = len(model_names) - 1
+            user_selected_model = st.selectbox(
+                "Select model",
+                options=model_names,
+                help=model_information,
+                index=best_default_model_index,
+            )
+            st.markdown(
+                "For more information on models and their costs, check the [OpenAI API Models page](https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo)"
+            )
 
             # Temperature selection
-            temperature_information = '''
+            temperature_information = """
             Temperature is a parameter passed into the LLM of choice. Higher temperature gives more random responses. A temperature of 0
             is deterministic.
-            '''
-            temperature = st.slider(label='Set temperature', min_value=0.0, max_value=1.0, step=0.01, help=temperature_information)
-            
+            """
+            temperature = st.slider(
+                label="Set temperature",
+                min_value=0.0,
+                max_value=1.0,
+                step=0.01,
+                help=temperature_information,
+            )
+
             # Toggle confirm submission after seeing cost (to be added later)
             # confirm_submission = st.checkbox('Toggle confirm submission?')
 
-    with st.form(key='valid_api_key'):
-        video_url = st.text_input(label='Enter the YouTube video link')
+    with st.form(key="valid_api_key"):
+        video_url = st.text_input(label="Enter the YouTube video link")
         if api_key:
-            submit_button = st.form_submit_button(label='Summarize')
+            submit_button = st.form_submit_button(label="Summarize")
         else:
-            st.warning('Please provide an OpenAI API key.')
-        
+            st.warning("Please provide an OpenAI API key.")
+
     if not submit_button:
         exit()
 
     loader = YoutubeLoader.from_youtube_url(
-        video_url, add_video_info=True, language=['en', 'cn'], translation='en'
+        video_url, add_video_info=True, language=["en", "cn"], translation="en"
     )
 
     # Load metadata
@@ -87,7 +104,12 @@ def main():
     if submit_button and youtube_metadata:
         try:
             st.subheader("Summary")
-            summary = generate_summary(metadata=str(youtube_metadata), api_key=api_key, temperature=temperature, model=user_selected_model)
+            summary = generate_summary(
+                metadata=str(youtube_metadata),
+                api_key=api_key,
+                temperature=temperature,
+                model=user_selected_model,
+            )
             markdown_summary = double_newlines(summary.content)
             st.markdown(markdown_summary)
 
@@ -95,6 +117,7 @@ def main():
             st.error(f"An error occurred: {e}")
     else:
         st.error("Invalid YouTube video link. Please make sure the link is correct.")
+
 
 if __name__ == "__main__":
     main()
